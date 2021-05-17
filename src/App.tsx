dcc4c89535cafd70fcac30f030836b2dc32a303a
Gabriel Nicutari <gabriel_nicutari@yahoo.ro>
2021-05-18 01:00:02 +0200

    Major Cleanup & Directory Organisation Refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import { Container } from 'semantic-ui-react';
-import NavBar from './app/layout/NavBar';
-import ActivityDashboard from './features/activities/dashboard/ActivityDashboard';
+import NavBar from './components/NavBar';
 import { observer } from 'mobx-react-lite';
 import { Route, Switch, useLocation } from 'react-router-dom';
-import HomePage from './features/home/HomePage';
-import ActivityForm from './features/activities/form/ActivityForm';
-import ActivityDetails from './features/activities/details/ActivityDetails';
-import TestErrors from './features/errors/TestError';
+import HomePage from './pages/home/HomePage';
 import { ToastContainer } from 'react-toastify';
-import NotFound from './features/errors/NotFound';
-import ServerError from './features/errors/ServerError';
-import ContactForm from "./features/ContactForm";
+import ContactForm from "./components/ContactForm";
 
 function App() {
   const location = useLocation();
@@ -27,13 +21,10 @@ function App() {
             <NavBar />
             <Container style={{ marginTop: '7em' }}>
               <Switch>
-                <Route exact path='/activities' component={ActivityDashboard} />
-                <Route path='/activities/:id' component={ActivityDetails} />
-                <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} />
-                <Route path='/errors' component={TestErrors} />
-                <Route path='/server-error' component={ServerError} />
+                <Route exact path='/activities'  />
+                <Route path='/activities/:id'  />
+                <Route key={location.key} path={['/createActivity', '/manage/:id']}  />
                 <Route path='/contactForm' component={ContactForm} />
-                <Route component={NotFound} />
               </Switch>
             </Container>
           </>
--- src/services/agent.ts
+++ src/services/agent.ts
@@ -1,8 +1,6 @@
 import axios, { AxiosError, AxiosResponse } from 'axios';
 import { toast } from 'react-toastify';
-import { history } from '../..';
-import { Activity } from '../models/activity';
-import { store } from '../stores/store';
+import { history } from '../index';
 
 const sleep = (delay: number) => {
     return new Promise((resolve) => {
@@ -41,7 +39,7 @@ axios.interceptors.response.use(async response => {
             history.push('/not-found');
             break;
         case 500:
-            store.commonStore.setServerError(data);
+            // store.commonStore.setServerError(data);
             history.push('/server-error');
             break;
     }
@@ -57,16 +55,16 @@ const requests = {
     del: <T>(url: string) => axios.delete<T>(url).then(responseBody),
 }
 
-const Activities = {
-    list: () => requests.get<Activity[]>('/activities'),
-    details: (id: string) => requests.get<Activity>(`/activities/${id}`),
-    create: (activity: Activity) => axios.post<void>('/activities', activity),
-    update: (activity: Activity) => axios.put<void>(`/activities/${activity.id}`, activity),
-    delete: (id: string) => axios.delete<void>(`/activities/${id}`)
-}
+// const Activities = {
+//     list: () => requests.get<Activity[]>('/activities'),
+//     details: (id: string) => requests.get<Activity>(`/activities/${id}`),
+//     create: (activity: Activity) => axios.post<void>('/activities', activity),
+//     update: (activity: Activity) => axios.put<void>(`/activities/${activity.id}`, activity),
+//     delete: (id: string) => axios.delete<void>(`/activities/${id}`)
+// }
 
 const agent = {
-    Activities
+    // Activities
 }
 
 export default agent;
