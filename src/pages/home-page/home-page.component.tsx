df4eaf43a9bd1562ddb6a6e67dfae01e8676c8ee
Cristian Ou <mccrys_00@yahoo.com>
2021-05-21 15:47:09 +0200

    Styling button + text

--- src/pages/home-page/home-page.component.tsx
+++ src/pages/home-page/home-page.component.tsx
@@ -6,16 +6,29 @@ import house from '../../assets/house.png'
 
 
 export default function HomePage() {
+    const buttonStyle = {
+        background: 'linear-gradient(90deg, #28cdcd 0%, rgba(0,42,57,0.6783088235294117) 100%)',
+        color: 'white',
+        height: 48,
+        padding: '0 100px',
+        margin: '20px 80px',
+      };
+
     return (
         <div className="home-page-container">
             <div className="title-banner">
                 <div className="left-box">
                     <div className="title-parag">NEW ENERGY SOLUTION FOR YOU</div>
-                    <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet sed vulputate vitae velit dictum cursus amet. Turpis donec ut velit quis. Cursus commodo, eget urna, sapien amet. </div>
-                    <Button >Read More</Button>
+                    <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet sed vulputate vitae velit dictum cursus amet. Turpis donec ut velit quis. Cursus commodo, eget urna, sapien amet.</div>
+                    <Button 
+                        style={buttonStyle} 
+                        variant="contained"
+                        href="#">
+                        Read More
+                    </Button>
                 </div>
                  
-                <img src={house} alt="house" />
+                <img src={house} alt="house" className="house-img"/>
             </div>
             <WeatherBanner />
             {/*<div className="small-banner"></div>*/}
