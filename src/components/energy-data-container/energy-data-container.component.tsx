67fdeed0db3ab2d716865cb2f08c003bbd5345b8
dim <dimitris.giokas1@gmail.com>
2021-06-01 16:15:02 +0200

    Added css grid and div container in the energy page. Also added component for containing each section of the page.

---
+++ src/components/energy-data-container/energy-data-container.component.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './energy-data-container.styles.scss';
+
+export default function EnergyDataContainer() {
+    return (<div>Energy Data Container</div>)
+}
--- src/pages/energy-page/energy-page.component.tsx
+++ src/pages/energy-page/energy-page.component.tsx
@@ -1,9 +1,18 @@
 import * as React from 'react';
+import './energy-page.styles.scss';
+import EnergyDataContainer from "../../components/energy-data-container/energy-data-container.component";
 
 export function EnergyPage() {
     return (
-        <div>
-            <h2>Energy page</h2>
+        <div className={"energy-page-container"}>
+            <div className={"left-energy-container"}>
+                <div className={"energy-consumption-past-days"}><EnergyDataContainer /></div>
+                <div className={"energy-consumption-today"}><EnergyDataContainer /></div>
+            </div>
+            <div className={"right-energy-container"}>
+                <div className={"where-money-goes"}><EnergyDataContainer /></div>
+                <div className={"energy-tips"}><EnergyDataContainer /></div>
+            </div>
         </div>
     );
-};
+}
