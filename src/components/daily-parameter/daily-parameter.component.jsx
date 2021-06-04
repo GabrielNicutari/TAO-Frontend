64a4d98023df830d4ea4ca3480cf31d364cadea1
Cristian Ou <mccrys_00@yahoo.com>
2021-06-04 08:25:01 +0200

    Right section

--- src/components/daily-parameter/daily-parameter.component.jsx
+++ src/components/daily-parameter/daily-parameter.component.jsx
@@ -2,18 +2,12 @@ import React from 'react';
 import './daily-parameter.styles.scss';
 
 export default function DailyReadingContainer({name, data, icon, measurementUnit}) {
-    console.log('drcontainer:', data)
+    // console.log('drcontainer:', data)
     return (
         <div className="parameter-box">
             <span>{icon}</span>
             <div className="parameter-name">{name}</div>
             <div className="parameter-value">{data} <span>{measurementUnit}</span></div>
         </div>
-        
-        // dailyReading.length > 0 ?
-        //     <div className="parameter-container">
-        //         <h4>{name}</h4>
-        //     </div>
-        // : <div>waiting</div>
     );
 }
--- src/pages/energy-page/energy-page.component.tsx
+++ src/pages/energy-page/energy-page.component.tsx
@@ -3,6 +3,7 @@ import './energy-page.styles.scss';
 import EnergyPageTodaySection from "../../components/energy-page-today-section/energy-page-today-section.component";
 import Chart from '../../components/chart/chart.component';
 import {useStore} from '../../stores/store';
+import ProgressBar from 'react-bootstrap/ProgressBar';
 
 export function EnergyPage() {
     const {dailyReadingStore, userStore: {isLoggedIn}} = useStore();
@@ -29,10 +30,18 @@ export function EnergyPage() {
                 <div className={"energy-consumption-today"}>
                     <EnergyPageTodaySection dailyReadings={dailyReadings}/>
                     </div>
-            </div>
+                </div>
             <div className={"right-energy-container"}>
-                <div className={"where-money-goes"}>Where does my money go?</div>
-                <div className={"energy-tips"}>Energy tips</div>
+                <div className={"where-money-goes"}>
+                    <h3>Where my money goes?</h3> 
+                    <h4>Energy</h4>
+                    <progress max={"7000"} value={"1512"} className="progress-bar"></progress>
+                    <h4>Water</h4>
+                    <progress max={"7000"} value={"2192"} className="progress-bar"></progress>
+                    <h4>Heat</h4>
+                    <progress max={"7000"} value={"4312"} className="progress-bar"></progress>
+                </div>
+                {/* <div className={"energy-tips"}>Energy tips</div> */}
             </div>
         </div>
         :
